fca74ca420e85a6ed13d634854467a9eae51b3b2
adamdrozdz <adamdrozdz@outlook.com>
2019-01-17 16:20:36 +0100

    fix splash

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -58,6 +58,7 @@ public class SplashScreen extends CordovaPlugin {
     private static ProgressDialog spinnerDialog;
     private static boolean firstShow = true;
     private static boolean lastHideAfterDelay; // https://issues.apache.org/jira/browse/CB-9094
+    private static boolean fadeOutAnimationInProgress = false;
 
     /**
      * Displays the splash drawable.
@@ -219,16 +220,20 @@ public class SplashScreen extends CordovaPlugin {
     private void removeSplashScreen(final boolean forceHideImmediately) {
         cordova.getActivity().runOnUiThread(new Runnable() {
             public void run() {
-        if (splashDialog != null && splashImageView != null && splashDialog.isShowing()) {//check for non-null splashImageView, see https://issues.apache.org/jira/browse/CB-12277
+                if (splashDialog != null && splashDialog.isShowing()) {
                     final int fadeSplashScreenDuration = getFadeDuration();
                     // CB-10692 If the plugin is being paused/destroyed, skip the fading and hide it immediately
-                    if (fadeSplashScreenDuration > 0 && forceHideImmediately == false) {
+                    if (   fadeSplashScreenDuration > 0
+                        && forceHideImmediately == false
+                        && !fadeOutAnimationInProgress
+                    ) {
                         AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
                         fadeOut.setInterpolator(new DecelerateInterpolator());
                         fadeOut.setDuration(fadeSplashScreenDuration);
 
                         splashImageView.setAnimation(fadeOut);
                         splashImageView.startAnimation(fadeOut);
+                        fadeOutAnimationInProgress = true;
 
                         fadeOut.setAnimationListener(new Animation.AnimationListener() {
                             @Override
@@ -238,10 +243,11 @@ public class SplashScreen extends CordovaPlugin {
 
                             @Override
                             public void onAnimationEnd(Animation animation) {
-                                if (splashDialog != null && splashImageView != null && splashDialog.isShowing()) {//check for non-null splashImageView, see https://issues.apache.org/jira/browse/CB-12277
+                                if (splashDialog != null && splashDialog.isShowing()) {
                                     splashDialog.dismiss();
                                     splashDialog = null;
                                     splashImageView = null;
+                                    fadeOutAnimationInProgress = false;
                                 }
                             }
 
@@ -254,6 +260,7 @@ public class SplashScreen extends CordovaPlugin {
                         splashDialog.dismiss();
                         splashDialog = null;
                         splashImageView = null;
+                        fadeOutAnimationInProgress = false;
                     }
                 }
             }
@@ -364,7 +371,7 @@ public class SplashScreen extends CordovaPlugin {
                 centeredLayout.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
                 ProgressBar progressBar = new ProgressBar(webView.getContext());
-                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(280, 280);
                 layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
                 progressBar.setLayoutParams(layoutParams);
 
